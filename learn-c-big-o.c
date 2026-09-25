5f2f2d23dbc77bfeb88b71e08b4313eb3ba1e129
AdamKirikisla <280528387+AdamKirikisla@users.noreply.github.com>
2026-09-25 19:53:34 -0400

    Variable init and output for number of terms in f(n)

--- learn-c-big-o.c
+++ learn-c-big-o.c
@@ -3,6 +3,15 @@
 int main()
 {
 
-    printf("Hello Big-O\n");
+    // Variable init
+    int nbTerms = 0;
+    int coefficent = 0;
+    int n0 = 0;
+
+    printf("\nE.g, f(n) = 2n + 5 + 3logn. This function has 3 terms.\n");
+    printf("How many terms are in the function: ");
+    scanf("%d", &nbTerms);
+    printf("F(n) has %d terms\n", nbTerms);
+
     return 0;
 }
